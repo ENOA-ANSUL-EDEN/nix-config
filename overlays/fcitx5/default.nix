522ee52ac7cdb6a5a04240a9216c3990be05bd66
ENOA-REIAH-UION <i@enoa.me>
2026-09-02 03:30:48 +0800

    fcitx5-rime: config data update

--- overlays/fcitx5/default.nix
+++ overlays/fcitx5/default.nix
@@ -1,15 +1,24 @@
-# 为了不使用默认的 rime-data，改用我自定义的方案数据，这里需要 override
-# 参考 https://github.com/NixOS/nixpkgs/blob/e4246ae1e7f78b7087dce9c9da10d28d3725025f/pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
-_:
-(_: super: {
-  # 不要覆盖默认内置配置，许多方案依赖基础配置(https://github.com/rime/rime-prelude)
-  # rime-data = ./rime-data-enoa;
+# 覆盖 nixpkgs 默认的 Rime 数据，使用自定义方案数据。
+# 参考： https://github.com/NixOS/nixpkgs/blob/e4246ae1e7f78b7087dce9c9da10d28d3725025f/pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
+_: (self: super: {
   fcitx5-rime = super.fcitx5-rime.override {
     rimeDataPkgs = [
-      super.rime-data
-      ./rime-data-enoa
-    ]; };
+      # nixpkgs 默认的 `rime-data` 包含 `rime-prelude` 等基础配置，
+      # 部分 Rime 方案可能依赖其中的文件。
+      #
+      # 在 nixpkgs commit 4b2b576b0efd28e9f8535119760cbd8dc9bac5bd 之前，
+      # 缺少 `default.yaml` 甚至可能导致 fcitx5-rime 无法正常工作。
+      #
+      # 如果自定义方案依赖这些基础配置，可以同时加入
+      # `super.rime-data` 和自定义数据；否则可以仅使用自定义数据。
+      #
+      # super.rime-data
 
-  # used by macOS Squirrel
-  flypy-squirrel = ./rime-data-enoa;
+      self.rime-data-custom
+    ];
+  };
+
+  # 导出自定义 Rime 数据，供不同平台和 Rime 前端复用。
+  # 例如 fcitx5-rime 和 macOS Squirrel。
+  rime-data-custom = ./rime-data-custom;
 })
